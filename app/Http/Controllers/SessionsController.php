387b9ef832224e31527498505a41b2b69a88c687
徐大帅 <coder.xsj@gmail.com>
2020-09-09 01:32:10 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    //
+    public function create(){
+        return view('sessions.create');
+    }
+    public function store(Request $request){
+            $auth = $this->validate($request,[
+                'email' => 'required|email|max:255',
+                'password' => 'required|'
+            ]);
+//            dd($auth);
+            if(Auth::attempt($auth)){
+                session()->flash('success', '欢迎回来');
+                return redirect()->route('users.show', [Auth::user()]);
+            } else {
+                session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+                return redirect()->back()->withInput();  //返回上一个路由，并且数据和{{ old('email') }} 对应
+            }
+//            return;
+    }
+}
